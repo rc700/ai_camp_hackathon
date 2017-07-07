e923f95892077b6f1f1d36c035cfbf5a41685108
Rachael Coulter <rcoulter700@googlemail.com>
2017-07-07 22:46:34 +0100

    Working (However inaccurate) Model
    CSV cleaned, null values removed, one hot encoded

--- Backend/ml_application.py
+++ Backend/ml_application.py
@@ -1,61 +1,108 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-
 from __future__ import print_function
 
-from pandas.plotting import scatter_matrix
-from pandas.plotting import andrews_curves
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-from sklearn.model_selection import train_test_split
+from pandas.plotting import scatter_matrix, andrews_curves
+
+from sklearn import preprocessing, metrics
 from sklearn.linear_model import SGDClassifier
-from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
-# read the data - set index (counter) to the first column
-data = pd.read_csv("iris_data.csv", index_col=0)
+# importing in the CSV
+data = pd.read_csv("survey_results_public.csv", index_col=0)
 
-print(data)
-print(data.values)
-print(data.shape)
-print(data.head())
-print(data.tail())
-print("* iris types:", data["Species"].unique(), sep="\n")
-data.describe()
-print(data.groupby('Species').size())
-
-# Visualise
+print(type(data))
 
 
-data.hist()
-plt.show()
+# cleaning the CSV
+clean_data = data.drop(['Professional', 'ProgramHobby', 'University', 'EmploymentStatus', 'CompanySize', 'CompanyType',
+                        'YearsCodedJobPast', 'WebDeveloperType', 'MobileDeveloperType', 'NonDeveloperType',
+                        'ExCoderReturn', 'ExCoderNotForMe', 'ExCoderBalance', 'ExCoder10Years', 'ExCoderBelonged',
+                        'ExCoderSkills',	'ExCoderWillNotCode', 'ExCoderActive', 'PronounceGIF', 'ProblemSolving',
+                        'BuildingThings', 'LearningNewTech', 'BoringDetails', 'JobSecurity', 'DiversityImportant',
+                        'AnnoyingUI', 'FriendsDevelopers', 'RightWrongWay', 'UnderstandComputers', 'SeriousWork',
+                        'InvestTimeTools', 'WorkPayCare', 'KinshipDevelopers', 'ChallengeMyself', 'CompetePeers',
+                        'ChangeWorld', 'JobSeekingStatus',	'HoursPerWeek',	'LastNewJob', 'AssessJobIndustry',
+                        'AssessJobRole', 'AssessJobExp',	'AssessJobDept', 'AssessJobTech', 'AssessJobProjects',
+                        'AssessJobCompensation',	'AssessJobOffice', 'AssessJobCommute',	'AssessJobRemote',
+                        'AssessJobLeaders', 'AssessJobProfDevel', 'AssessJobDiversity', 'AssessJobProduct',
+                        'AssessJobFinances', 'ImportantBenefits', 'ClickyKeys', 'JobProfile', 'ResumePrompted',
+                        'LearnedHiring', 'ImportantHiringAlgorithms', 'ImportantHiringTechExp',
+                        'ImportantHiringCommunication', 'ImportantHiringOpenSource', 'ImportantHiringPMExp',
+                        'ImportantHiringCompanies',	'ImportantHiringTitles', 'ImportantHiringEducation',
+                        'ImportantHiringRep', 'ImportantHiringGettingThingsDone', 'Currency', 'Overpaid', 'TabsSpaces',
+                        'EducationImportant', 'EducationTypes', 'SelfTaughtTypes', 'TimeAfterBootcamp',
+                        'CousinEducation', 'WorkStart', 'HaveWorkedLanguage', 'WantWorkLanguage', 'HaveWorkedFramework',
+                        'WantWorkFramework', 'HaveWorkedDatabase', 'WantWorkDatabase', 'HaveWorkedPlatform',
+                        'WantWorkPlatform', 'IDE', 'AuditoryEnvironment', 'Methodology', 'VersionControl',
+                        'CheckInCode', 'ShipIt', 'OtherPeoplesCode', 'ProjectManagement', 'EnjoyDebugging', 'InTheZone',
+                        'DifficultCommunication', 'CollaborateRemote', 'MetricAssess', 'EquipmentSatisfiedMonitors',
+                        'EquipmentSatisfiedCPU', 'EquipmentSatisfiedRAM', 'EquipmentSatisfiedStorage',
+                        'EquipmentSatisfiedRW', 'InfluenceInternet', 'InfluenceWorkstation', 'InfluenceHardware',
+                        'InfluenceServers', 'InfluenceTechStack', 'InfluenceDeptTech', 'InfluenceVizTools',
+                        'InfluenceDatabase', 'InfluenceCloud', 'InfluenceConsultants', 'InfluenceRecruitment',
+                        'InfluenceCommunication', 'StackOverflowDescribes', 'StackOverflowSatisfaction',
+                        'StackOverflowDevices', 'StackOverflowFoundAnswer',	'StackOverflowCopiedCode',
+                        'StackOverflowJobListing', 'StackOverflowCompanyPage', 'StackOverflowJobSearch',
+                        'StackOverflowNewQuestion', 'StackOverflowAnswer', 'StackOverflowMetaChat',
+                        'StackOverflowAdsRelevant',	'StackOverflowAdsDistracting',	'StackOverflowModeration',
+                        'StackOverflowCommunity',	'StackOverflowHelpful',	'StackOverflowBetter',
+                        'StackOverflowWhatDo',	'StackOverflowMakeMoney', 'HighestEducationParents', 'Race',
+                        'SurveyLong', 'QuestionsInteresting', 'QuestionsConfusing', 'InterestedAnswers', 'Salary',
+                        'ExpectedSalary'], axis=1)
 
-# can see correlation between two sets of features
-scatter_matrix(data)
-plt.show()
 
-andrews_curves(data, 'Species')
-plt.show()
+# Remove null/NA values
+clean_data.fillna("NaN")
+clean_data = clean_data.dropna(axis='rows', how='any')
+print('before', clean_data.head())
 
-# Split into training + Testing
+# One Hot Encoding
+# Find all categorical data
+categoricals = clean_data.select_dtypes(include=[object])
+print(categoricals.columns)
+
+# 'Country', 'FormalEducation', 'MajorUndergrad', 'HomeRemote', 'YearsProgram',
+# 'YearsCodedJob', 'DeveloperType', 'Gender' need one hot encoded
+# Create a LabelEncoder object and fit it to each feature in X
+# 1. INSTANTIATE
+# encode labels with value between 0 and n_classes-1.
+le = preprocessing.LabelEncoder()
+# 2/3. FIT AND TRANSFORM
+# use df.apply() to apply le.fit_transform to all columns
+clean_data = clean_data.apply(le.fit_transform)
+print('after', clean_data.head())
 
 
-values = data.values
 
-# gets the feature values - gets all types sliced from structure in range of column 0 to 4
-X = values[:,0:4]
-# Gets the labels - gets tuple sliced from structure, column 4 only
-Y = values[:,4]
 
-# print(values[:5])
-# print(X[:5])
-# print(Y[:5])
-# print(data[:5])
+
+# All headers/features
+# print('There are this many features: ', len((list(clean_data))))
+
+# All unique job types
+# print("Job Types: ", clean_data["DeveloperType"].unique(), sep="\n")
+
+# # Split into training + Testing
+
+values = clean_data.values
+
+# # gets the feature values - all except the Developer Types
+X = values[:, [0, 1, 2, 3, 4, 5, 7, 8, 9]]
+
+# # Gets the labels - gets tuple sliced from structure, column 4 only
+Y = values[:, 6]
+
 
 validation_size = 0.20
 seed = 7
 X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
-# print(X_validation[:5])
-# print(Y_validation[:5])
+print(X_validation[:5])
+print(Y_validation[:5])
 
 # Build the model - svm (students should not use on vm)
 
@@ -81,6 +128,6 @@ print("Accuracy:\n%s" % str(float(num_correct) / float(len(Y_validation))))
 
 # use report
 print("Accuracy:\n%s" % metrics.accuracy_score(Y_validation, predicted))
-print("Classification report for classifier %s:\n%s\n"
-      % (classifier, metrics.classification_report(Y_validation, predicted)))
+# print("Classification report for classifier %s:\n%s\n"
+#       % (classifier, metrics.classification_report(Y_validation, predicted)))
 print("Confusion matrix:\n%s" % metrics.confusion_matrix(Y_validation, predicted))
