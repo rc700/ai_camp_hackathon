f7fde73a8b84e88765cb0e31f0c4f087ab232a60
laurentaylor <ltaylor26@qub.ac.uk>
2017-07-08 12:07:45 +0100

    index.js initial work

--- Chat_Bot/index.js
+++ Chat_Bot/index.js
@@ -1,15 +1,12 @@
 // dependencies
 var restify = require('restify');
 var builder = require('botbuilder');
-var request = require('request');
-var natural = require('natural');
 
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
@@ -21,49 +18,213 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 
 // Listen for messages from users 
-server.post('/placeholder', connector.listen());
+server.post('/hack', connector.listen());
 
-//entry point for branches and receving an image
+// This bot enables users to either make a dinner reservation or order dinner.
 var bot = new builder.UniversalBot(connector, function(session){
-
+	session.send("Welcome to PREDICT MY IT FUTURE")
+    var msg = "Want to find out your CAREER IN IT?";
+    session.send(msg);
 });
 
-//placeholder dialog
-bot.dialog('placeholder', [
-	function (session) {
-
+// Dinner menu
+var dinnerMenu = {
+    "Potato salad - $5.99":{
+        Description: "Salad",
+        Price: 5.99
     },
-    function (session, results) {
-
+    "Tuna sandwich - $6.89":{
+        Description: "Sticky toffee pudding",
+        Price: 6.89
+    },
+    "Clam Chowder - $4.50":{
+        Description: "Malteaser cheesecake",
+        Price: 4.50
     }
-]).triggerAction({
-    matches: /^placeholder$/i
+};
+
+// Help
+bot.dialog('help', function(session){
+    // Send message
+    session.endDialog('To talk with the bot say Hello');
+}).triggerAction({
+    matches : /^help$/,
+    onSelectAction: (session, args, next) => {
+        session.beginDialog(args.action, args);
+    }
 });
 
+// Dinner reservation
+bot.dialog('dinner reservation', [function(session){
+    // Send message
+    session.send('Let us make a dinner reservation');
+    session.beginDialog("askForDateTime")
+    
+},
+function (session, results){
+session.dialogData.reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
+session.beginDialog("askForPartySize")
+},
+function (session, results){
+    if(results.response){
+        session.dialogData.partySize = results.response
+    }
+    session.beginDialog("askForReserverName")
+},
+function (session, results){
+    if(results.response){
+        session.dialogData.reserverName = results.response
+        session.send("Please confirm your booking details:<br/><br/>Date: %s <br/>Party size: %s <br/>ReserverName: %s",
+        session.dialogData.reservationDate, session.dialogData.partySize, session.dialogData.reserverName)
 
-//get request example
-//usage
-//    	 getData('', function (msg) {
-//            console.log(msg)
-//         });
-function getData(data, cb){
-	request('http://localhost:5000/placeholder_get', function (error, response, body) {
-		cb(body);
-	});
+        endDialog();
+    }
 }
 
-//post request example
-//you can also post images! if sending image form a chatbot use 'fs' module to load the image url and then send it.
-//usage
-//    	 postData('data', function (msg) {
-//             console.log(msg)
-//         });
+]).triggerAction({
+    matches : /^dinner reservation$/
+})
 
-function postData(data, cb){
-	request.post({
-  	url:     'http://localhost:5000/placeholder_post',
- 	 	body:    data
-	}, function(error, response, body){
-  	   	cb(body)
-	});
-}
+// Gets date
+bot.dialog("askForDateTime",[
+    function(session){
+        builder.Prompts.time(session, "Please provide a reservation time - i.e. \"June 6th at 4pm\"")
+    },
+    function (session, results){
+        session.endDialogWithResult(results)
+    }
+])
+
+// Gets party size
+bot.dialog("askForPartySize",[
+    function(session){
+        builder.Prompts.text(session, "What is the party size?")
+    },
+    function (session, results){
+        session.endDialogWithResult(results)
+    }
+])
+
+// Get reserver name
+bot.dialog("askForReserverName",[
+    function(session){
+        builder.Prompts.text(session, "What is your name?")
+    },
+    function (session, results){
+        session.endDialogWithResult(results)
+    }
+])
+
+// Order dinner
+bot.dialog('order dinner', [
+    function(session){
+        session.send("Let\'s order dinner");
+        builder.Prompts.choice(session, "Dinner Menu!", dinnerMenu)
+    },
+    function(session, results){
+        if(results.response){
+            var order = dinnerMenu[results.response.entity];
+            var msg = "You have ordered: %(Description)s for the glorious price of $%(Price)f."
+            
+            if(order.Description == "Salad"){
+                session.send("you have a problem.")
+            }
+            
+            session.send("Solid choice.")
+            session.dialogData.order = order;
+            session.send(msg, order)
+            builder.Prompts.text(session, "What is your room number?")
+        }
+    },
+
+    function(session, results){
+        if(results.response){
+            session.dialogData.room = results.response;
+            var msg = "Thank you. Your order will be delivered to room #%s";
+            session.endConversation(msg, session.dialogData.room)
+        }
+    } 
+
+]).triggerAction({
+    matches : /^order dinner$/,
+})
+.endConversationAction(
+    "endOrderDinner", "Ok, goodbye.",
+    {
+        matches: /^cancel$|^goodbye$/i,
+        confirmPrompt: "This will cancel your order. Are you sure?"
+    }
+)
+
+// Get country
+bot.dialog('get country', [
+    function(session){
+        session.send("Let\'s get started!");
+		builder.Prompts.text(session, "What country are you from?")
+        //builder.Prompts.choice(session, "Dinner Menu!", dinnerMenu)
+    },
+    function(session, results){
+        if(results.response){
+			var msg = session.dialogData.Country = results.response;
+			session.send("You are from " + msg)
+
+            builder.Prompts.text(session, "What is your formal education?")
+        }
+    },
+
+	function(session, results){
+			if(results.response){
+				var msg = session.dialogData.FormalEducation = results.response;
+				session.send("Your formal education is: " + msg)
+
+				builder.Prompts.text(session, "What is your major undergrad?")
+			}
+		},
+
+	function(session, results){
+			if(results.response){
+				var msg = session.dialogData.MajorUndergrad = results.response;
+				session.send("Your major undergrad is: " + msg)
+
+				builder.Prompts.text(session, "Would you rather work at home or remotely?")
+			}
+		},
+
+	function(session, results){
+			if(results.response){
+				var msg = session.dialogData.homeRemote = results.response;
+				session.send("You prefer : " + msg)
+
+				builder.Prompts.text(session, "How many years of programming experience do you have? (non-professional)")
+			}
+		},
+
+	function(session, results){
+			if(results.response){
+				var msg = session.dialogData.YearsProgram = results.response;
+				session.send("You have " + msg + " years of programming experience")
+
+				builder.Prompts.text(session, "How many years of professional programming experience do you have?")
+			}
+		},
+
+    function(session, results){
+        if(results.response){
+            session.dialogData.YearsCodedJob = results.response;
+            var msg = "You have #%s years of professional programming experience";
+			session.send("testtttt" + session.dialogData.homeRemote)
+			// GET PREDICTION & OUTPUT IT
+            session.endConversation(msg, session.dialogData.YearsCodedJob)
+        }
+    } 
+
+]).triggerAction({
+    matches : /^yes$|^ok$|^okay$|^yeah$|^yas$|^aye$/i,
+})
+.endConversationAction(
+    "endOrderDinner", "Ok, goodbye.",
+    {
+        matches: /^cancel$|^goodbye$/i,
+        confirmPrompt: "This will cancel your order. Are you sure?"
+    }
+)
