0395265b803f0771af57c5ad84459b6fa430dde3
mattheworr <mattheworr1@hotmail.co.uk>
2017-07-08 15:13:08 +0100

    CLeaned up server.py

--- Backend/server.py
+++ Backend/server.py
@@ -3,19 +3,12 @@ import ml_application as ml
 
 app = Flask(__name__)
 
+# Going from Chatbot to Model
 @app.route('/predict', methods=['POST'])
 def post_from_application():
     aList = eval(request.form.getlist('list')[0])
     predict = ml.predict(aList)
     return predict
 
-@app.route('/posting', methods=['POST'])
-def posting():
-
-    return 
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-# Test the get - curl -X GET http://localhost:5000/placeholder_get
-# test the post curl -d "test" -X POST http://localhost:5000/placeholder_post
