90de3d0c5b27003084ef28a7aeab44e2c40fe7c0
Rachael Coulter <rcoulter700@googlemail.com>
2017-07-08 12:11:08 +0100

    Reducing no of job types, removing invalid

--- Backend/ml_application.py
+++ Backend/ml_application.py
@@ -1,20 +1,15 @@
 from __future__ import print_function
 
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 
-from pandas.plotting import scatter_matrix, andrews_curves
-
-from sklearn import preprocessing, metrics
+from sklearn import preprocessing
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 
 # importing in the CSV
 data = pd.read_csv("survey_results_public.csv", index_col=0)
 
-print(type(data))
+# print(type(data))
 
 
 # cleaning the CSV
@@ -58,40 +53,47 @@ clean_data = data.drop(['Professional', 'ProgramHobby', 'University', 'Employmen
 # Remove null/NA values
 clean_data.fillna("NaN")
 clean_data = clean_data.dropna(axis='rows', how='any')
-print('before', clean_data.head())
+
+# Remove rows with invalid job types
+clean_data = clean_data[~(clean_data['DeveloperType'] != 'Other' or 'Web Developer' or
+                          'Embedded applications/devices developer' or 'Mobile developer' or
+                          'Desktop applications developer' or 'DevOps specialist' or 'Quality assurance engineer' or
+                          'Machine learning specialist' or 'Desktop applications developer' or 'Graphics programming' or
+                          'Database administrator' or 'Systems administrator').any(axis=1)]
+
+print(clean_data.head(30))
+
+# Create Array of Unique Jobs
+unique_jobs = clean_data['DeveloperType'].unique()
+print(unique_jobs[:100])
+
+# Create Job Dictionary to associate job with id
+job_dict = {}
+counter = len(unique_jobs) - 1
+while counter >=0:
+    job_dict[counter] = unique_jobs[counter]
+    counter = counter - 1
+
+# Replace all values in DeveloperType with dictionary key
+
+
 
 # One Hot Encoding
 # Find all categorical data
 categoricals = clean_data.select_dtypes(include=[object])
-print(categoricals.columns)
 
-# 'Country', 'FormalEducation', 'MajorUndergrad', 'HomeRemote', 'YearsProgram',
-# 'YearsCodedJob', 'DeveloperType', 'Gender' need one hot encoded
-# Create a LabelEncoder object and fit it to each feature in X
-# 1. INSTANTIATE
-# encode labels with value between 0 and n_classes-1.
+# Create a LabelEncoder object and fit it to each feature
 le = preprocessing.LabelEncoder()
-# 2/3. FIT AND TRANSFORM
-# use df.apply() to apply le.fit_transform to all columns
+
+# apply le.fit_transform to all columns
 clean_data = clean_data.apply(le.fit_transform)
-print('after', clean_data.head())
-
-
-
-
-
-# All headers/features
-# print('There are this many features: ', len((list(clean_data))))
-
-# All unique job types
-# print("Job Types: ", clean_data["DeveloperType"].unique(), sep="\n")
 
 # # Split into training + Testing
 
 values = clean_data.values
 
 # # gets the feature values - all except the Developer Types
-X = values[:, [0, 1, 2, 3, 4, 5, 7, 8, 9]]
+X = values[:, [0, 1, 2, 3, 4, 5]]
 
 # # Gets the labels - gets tuple sliced from structure, column 4 only
 Y = values[:, 6]
@@ -101,33 +103,12 @@ validation_size = 0.20
 seed = 7
 X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size, random_state=seed)
 
-print(X_validation[:5])
-print(Y_validation[:5])
-
 # Build the model - svm (students should not use on vm)
-
-
 classifier = SGDClassifier()
 classifier.fit(X_train, Y_train)
 
 # Predict the results
+me = [1, 2, 3, 4, 5, 6]
 
-predicted = classifier.predict(X_validation)
-print(predicted)
-
-# Get Results
-
-
-#manual
-num_correct = 0
-for i in range(0, len(Y_validation)):
-    if Y_validation[i] == predicted[i]:
-        num_correct = num_correct + 1
-
-print("Accuracy:\n%s" % str(float(num_correct) / float(len(Y_validation))))
-
-# use report
-print("Accuracy:\n%s" % metrics.accuracy_score(Y_validation, predicted))
-# print("Classification report for classifier %s:\n%s\n"
-#       % (classifier, metrics.classification_report(Y_validation, predicted)))
-print("Confusion matrix:\n%s" % metrics.confusion_matrix(Y_validation, predicted))
+predicted = classifier.predict(me)
+# print(clean_data['DeveloperType'].unique())
